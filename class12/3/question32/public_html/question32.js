e5d1fc137e0c264877409f8e77f7684a44ac6620
Arthur <Arthur@Arthur-Laptop.niacc.edu>
2020-12-17 08:37:36 -0600

    Starting question 32

---
+++ class12/3/question32/public_html/question32.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+let dailyOne = "Day one sales total";
+let dailyTwo = "Day two sales total";
+let dailyThree = "Day three sales total";
+let dailyFour = "Day four sales total";
+let dailyFive = "Day five sales total";
+let dailySix = "Day six sales total";
+let dailySeven = "Day seven sales total";
+
+let salesTotal = [dailyOne, dailyTwo, dailyThree, dailyFour, dailyFive, dailySix, dailySeven];
+
+let weeklyAverage = salesTotal/7;
+
